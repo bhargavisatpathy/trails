ad293966e6be708c2ccd44e91e25c34c2474c347
Lydias303 <shaferlydia@yahoo.com>
2015-04-06 21:12:45 -0600

    Update trails_controller_test.rb

--- test/controllers/trails_controller_test.rb
+++ test/controllers/trails_controller_test.rb
@@ -3,10 +3,10 @@ require 'test_helper'
 class Api::V1::Trails::TrailsControllerTest < ActionController::TestCase
 
   test "should get trails index" do
-
     get :index
 
     data = JSON.parse(response.body)
+    
     assert_response :success
     assert_kind_of Hash, data
   end
@@ -16,6 +16,7 @@ class Api::V1::Trails::TrailsControllerTest < ActionController::TestCase
     get :show, id: trail.id
 
     data = JSON.parse(response.body)
+    
     assert_response :success
     assert_kind_of Hash, data
   end
