608d64ce572ba7269d6c59f1e52e469e29ea5892
Lydia Shafer <shaferlydia@yahoo.com>
2015-04-08 11:35:18 -0600

    Update trails_controller_test.rb

--- test/controllers/trails_controller_test.rb
+++ test/controllers/trails_controller_test.rb
@@ -60,7 +60,7 @@ class Api::V1::TrailsControllerTest < ActionController::TestCase
                          lng: -103.23,
                          description: "A darn good time",
                          directions: "take a right")
-    species = Species.create(kingdom: "Animalia",
+    Species.create(kingdom: "Animalia",
                              trail_id: trail.id,
                              scientific_name: "Parus caeruleus",
                              photo_url: "http://media.eol.org/content/2014/12/28/22/94944_orig.jpg",
