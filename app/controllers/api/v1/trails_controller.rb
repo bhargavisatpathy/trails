4c324fd672b64f9d20067bb5040260b7399c7c98
Bhargavi Satpathy <bhargavisatpathy@gmail.com>
2015-04-13 23:53:51 -0600

    search by state done

--- app/controllers/api/v1/trails_controller.rb
+++ app/controllers/api/v1/trails_controller.rb
@@ -12,4 +12,15 @@ class Api::V1::TrailsController < ApplicationController
 
     respond_with @trail
   end
+
+  def search
+    if !params[:state].nil?
+      @trails = Trail.filter_by_state(params[:state])
+    elsif !params[:lat].nil? && !params[:lng].nil?
+      @trails = Trail.filter_by_lat_lng(params[:lat].to_f, params[:lng].to_f)
+      byebug
+    end
+
+    respond_with @trails
+  end
 end
